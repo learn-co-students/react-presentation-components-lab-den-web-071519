d1d8d869ab40488c50f2602f6ee6301090cf20a7
Ethan Collins <ethantcollins98@gmail.com>
2019-09-19 09:55:30 -0600

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,18 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = () => {
+
+    function handleClick(event){
+        event.target.textContent == 'I am just happy'
+            ? event.target.textContent = `I'M HAPPY OKAY!!`
+            : event.target.textContent = 'I am just happy'
+    }
+
+
+    return (
+        <div onClick={(event) => handleClick(event)}>I am just happy</div>
+    )
+}
+
+export default SimplerComponent
